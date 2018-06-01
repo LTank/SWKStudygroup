6e842fb308f09de8a064b1db45bd547ddf122b26
T.K. Thomsen <tobyrius@gmail.com>
2018-06-01 11:39:28 +0200

    webserver fix

--- ClientServer2/CustomWebserver/src/com/webserver/Webserver.java
+++ ClientServer2/CustomWebserver/src/com/webserver/Webserver.java
@@ -19,7 +19,7 @@ public class Webserver {
     6. Luk
      */
 
-    static String path = System.getProperty("user.dir") + "/"; // giver roden til projektmappen
+    static String path = System.getProperty("user.dir") + "/ClientServer2/CustomWebserver/"; // giver roden til projektmappen
     public static void main(String[] args) {
 
         try {
